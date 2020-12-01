b5911d855ab0031ba1a1ab529c773919e54ebb7a
Sebl29 <portal@datenwei.de>
2020-12-01 18:02:14 +0100

    forked, updated dependencies; deployment of v2.2.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Server Side Rendering for Meteor with Blaze",
-  "version": "2.2.1",
+  "version": "2.2.2",
   "git": "https://github.com/meteorhacks/meteor-ssr",
   "name": "corefihub:meteorhacks-ssr"
 });
